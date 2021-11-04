62b76bdd63ebaba34436864f8c40d0020b7c3c40
witpanta <76709428+witpanta@users.noreply.github.com>
2021-11-04 18:37:01 +0100

    git init

---
+++ Scanner_Code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Scanner_Code{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your name?\n");
+        String name = input.nextLine();
+
+        System.out.print("How old are you?\n");
+        int age = input.nextInt();
+
+        System.out.print("Your name is: " + name + "\nYou are " + age + " years old.\nNext year you will be " + (age+1) + " years old.\n");
+    }
+}
